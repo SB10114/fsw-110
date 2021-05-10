60476db2a63970ef9cbfdf54bbd4cb6d7d3abd14
Sierra <sierra.butler@bryanuniversity.edu>
2021-05-10 18:34:22 -0500

    first commit.5

---
+++ week1/selecting Elements/app.js
@@ -0,0 +1,10 @@
+// var h = document.createElement("h1");
+// var t = document.createTextNode('My text');
+// h.appendChild(t);
+
+
+
+var howdy = document.getElementById("howdy")
+howdy.textContent += "Hello there, General Kenobi..."
+console.log(howdy)
+console.dir(howdy)
