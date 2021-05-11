1fda873d626f8ec2c46508a5cac31f61c7f6942e
Sierra <sierra.butler@bryanuniversity.edu>
2021-05-10 21:39:38 -0500

    commit

--- week2/creating&AppendingElements.js
+++ week2/creating&AppendingElements.js
@@ -4,10 +4,10 @@ newh1.textContent = "Welcome to my JS site"
 var newP = document.createElement("p")
 newP.textContent = "All of this was created with JavaScript"
 
-var newli = document.createElement("li")
-newli.textContent ="Dwight Shrute"
+// var newlist = document.createElement("list")
+// newlist.textContent = "Dwight Shrute"
 
-document.body.append(newh1)
-document.body.append(newP)
-document.body.prepend(newli)
+document.body.prepend(newh1)
+document.body.appendChild(newP)
+//document.body.prepend(newlist)
 
